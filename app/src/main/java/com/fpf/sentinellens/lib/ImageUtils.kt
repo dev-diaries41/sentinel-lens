6771f7c430a3a6402b670e8eaf987345171b2b64
d41dev <d41dev@proton.me>
2025-09-25 17:18:24 +0100

    update: remove uncessary functions

--- app/src/main/java/com/fpf/sentinellens/lib/ImageUtils.kt
+++ app/src/main/java/com/fpf/sentinellens/lib/ImageUtils.kt
@@ -5,62 +5,11 @@ import android.graphics.*
 import android.media.Image
 import android.net.Uri
 import android.util.LruCache
-import java.nio.FloatBuffer
-import androidx.core.graphics.scale
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.ByteArrayOutputStream
 import java.io.IOException
 
-const val DIM_BATCH_SIZE = 1
-const val DIM_PIXEL_SIZE = 3
-const val IMAGE_SIZE_X = 160
-const val IMAGE_SIZE_Y = 160
-
-fun preProcess(bitmap: Bitmap): FloatBuffer {
-    val imgData = FloatBuffer.allocate(
-        DIM_BATCH_SIZE
-                * DIM_PIXEL_SIZE
-                * IMAGE_SIZE_X
-                * IMAGE_SIZE_Y
-    )
-    imgData.rewind()
-    val stride = IMAGE_SIZE_X * IMAGE_SIZE_Y
-    val bmpData = IntArray(stride)
-    bitmap.getPixels(bmpData, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
-    for (i in 0..IMAGE_SIZE_X - 1) {
-        for (j in 0..IMAGE_SIZE_Y - 1) {
-            val idx = IMAGE_SIZE_Y * i + j
-            val pixelValue = bmpData[idx]
-            imgData.put(idx, (((pixelValue shr 16 and 0xFF) / 255f - 0.485f) / 0.229f))
-            imgData.put(idx + stride, (((pixelValue shr 8 and 0xFF) / 255f - 0.456f) / 0.224f))
-            imgData.put(idx + stride * 2, (((pixelValue and 0xFF) / 255f - 0.406f) / 0.225f))
-        }
-    }
-
-    imgData.rewind()
-    return imgData
-}
-
-fun centerCrop(bitmap: Bitmap, imageSize: Int): Bitmap {
-    val cropX: Int
-    val cropY: Int
-    val cropSize: Int
-    if (bitmap.width >= bitmap.height) {
-        cropX = bitmap.width / 2 - bitmap.height / 2
-        cropY = 0
-        cropSize = bitmap.height
-    } else {
-        cropX = 0
-        cropY = bitmap.height / 2 - bitmap.width / 2
-        cropSize = bitmap.width
-    }
-    var bitmapCropped = Bitmap.createBitmap(
-        bitmap, cropX, cropY, cropSize, cropSize
-    )
-    bitmapCropped = bitmapCropped.scale(imageSize, imageSize, false)
-    return bitmapCropped
-}
 
 fun cameraImageToBitmap(image: Image, rotationDegrees: Int = 0): Bitmap {
     // Get YUV planes from the image
