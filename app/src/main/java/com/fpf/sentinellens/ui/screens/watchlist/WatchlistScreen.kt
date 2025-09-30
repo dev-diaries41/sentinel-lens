68d8ef5fba2cabc90729d6fc8d213dc120eb9ecc
d41dev <d41dev@proton.me>
2025-09-30 21:35:32 +0100

    update: remove add person button in emptyscreen

--- app/src/main/java/com/fpf/sentinellens/ui/screens/watchlist/WatchlistScreen.kt
+++ app/src/main/java/com/fpf/sentinellens/ui/screens/watchlist/WatchlistScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
