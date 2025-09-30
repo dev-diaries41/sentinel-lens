40aa79259bbe7ed2a2b6b1b99007f74585b2a36f
d41dev <d41dev@proton.me>
2025-09-30 22:48:49 +0100

    update: handle updating all faces with ids which point to picker uri -> local uri

--- app/src/main/java/com/fpf/sentinellens/ui/screens/watchlist/WatchlistViewModel.kt
+++ app/src/main/java/com/fpf/sentinellens/ui/screens/watchlist/WatchlistViewModel.kt
@@ -7,12 +7,24 @@ import androidx.lifecycle.viewModelScope
 import com.fpf.sentinellens.data.faces.Face
 import com.fpf.sentinellens.data.faces.FaceDatabase
 import com.fpf.sentinellens.data.faces.FacesRepository
+import com.fpf.sentinellens.lib.Storage
 import com.fpf.sentinellens.lib.deleteLocalFile
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class WatchlistViewModel(application: Application) : AndroidViewModel(application)  {
     private val repository: FacesRepository = FacesRepository(FaceDatabase.getDatabase(application).faceDao())
     val faceList: LiveData<List<Face>> =  repository.allFaces
+    val storage = Storage.getInstance(application)
+
+    init {
+        val hasUpdatedIds = storage.getItem("hasUpdatedIds") != null
+        if(!hasUpdatedIds){
+            viewModelScope.launch(Dispatchers.IO){
+                repository.migratePickerUrisToLocal(application) { storage.setItem("hasUpdatedIds", true.toString()) }
+            }
+        }
+    }
 
     fun deleteFace(id: String){
         viewModelScope.launch {
